97d5697052638f66904df2d5400c29bd4e126b5d
Thiago Antonio <115923226+thiago-antonio@users.noreply.github.com>
2023-05-22 14:47:13 -0300

    exercicio curso de js

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro ${vel}km/h`)
+if(vel > 60) { // condicao simples!
+    console.log(`voce ultrapassou a velicidade permitida. MULTADO`)
+}
+console.log(`Dirija sempre usando sinto de seguranca`)
